db1979a6a88f3196c6d9a0bbe16ff2a97a8a83fb
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 12:52:10 -0300

    Add files via upload

---
+++ AULA08A.py
@@ -0,0 +1,26 @@
+###  EXEMPLOS DE MÓDULOS A SEREM UTILIZADOS  ###
+ #import math
+ #num = int(input('digite um numero :'))
+ #raiz = math.sqrt(num)
+ #print('A raiz de {} é igual a {}'.format(num, raiz)) #NÚMERO EXATO
+ #print('A raiz de {} é igual a {:.2f}'.format(num, raiz)) #:.2F NÚMERO EXATO COM 2 CASAS DECIMAIS
+ #print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz))) #'CEIL' ARREDONDA PRA CIMA
+ #print('A raiz de {} é igual a {}'.format(num, math.floor(raiz))) #'FLOOR' ARREDONDA PRA BAIXO
+
+ #from math import sqrt, floor #('FROM' -  IMPORTA NA BIBLIOTECA APENAS a FUNÇÃO QUE FOR USAR)
+ #num=int(input( 'digite um número :'))
+ #raiz = sqrt(num)
+ #print('A raiz de {} é igual a {}'.format(num,floor,raiz))
+
+ #import random
+ #num = random.random() #(NUMERO ALEATÓRIO ATÉ 1)
+ #print(num)
+
+ #import random
+ #num = random.randint(1, 10) #(NÚMERO ALEATORIO DE 1 A 10)
+ #print(num)
+
+ #import emoji
+ #print(emoji.emojize('Obrigado :alien:', use_aliases=True))
+
+###  EXEMPLOS DE MÓDULOS A SEREM UTILIZADOS  ###
